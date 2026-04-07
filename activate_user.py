f008eb23cda2d33052f8e2b6d7a49b0495ac82ee
maingi <sammaingi5@gmail.com>
2026-04-07 09:47:48 +0300

    activate user added

--- activate_user.py
+++ activate_user.py
@@ -1,10 +1,12 @@
 from models import db, User
 
 email = input("Enter the email of the user to activate: ").strip()
+role = input("Enter the Role: ").strip()
 user = User.query.filter_by(email=email).first()
 if user:
     user.status = "active"
+    user.role = role
     db.session.commit()
-    print(f"{email} is now active")
+    print(f"{email} is now active and Admin")
 else:
     print(f"User {email} not found")
